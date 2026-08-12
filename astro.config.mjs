3a0bb8a2e23311f5c593871e7e6969d997c87ca4
Hernan Marcos Romero Villca <hernan125476@gmail.com>
2026-08-12 12:33:31 -0400

    fix: install and configure @astrojs/vercel adapter for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
   output: 'server',
+  adapter: vercel(),
 });
+
